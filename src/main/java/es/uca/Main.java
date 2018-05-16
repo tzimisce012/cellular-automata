a958da6be11965639b14088eaec4acfe8030304d
Daniel Ochoa <dochoaro@kent.edu>
2018-05-16 20:42:08 +0200

    Improve printer class

--- src/main/java/es/uca/Main.java
+++ src/main/java/es/uca/Main.java
@@ -1,6 +1,5 @@
 package es.uca;
 
-import com.diogonunes.jcdp.color.api.Ansi;
 import es.uca.rules.Rule;
 import es.uca.rules.RuleFactory;
 import es.uca.util.Printer;
@@ -9,11 +8,11 @@ public class Main {
 
   public static void main(String[] args) {
 
-    int size = 95;
-    int numberOfIterations = 95;
+    int size = 99;
+    int numberOfIterations = 48;
 
     int[] cells = new int[size];
-    int[][] result = new int[size][numberOfIterations];
+    int[][] result = new int[numberOfIterations][numberOfIterations];
 
     for (int i = 0; i < cells.length; i++) {
       cells[i] = 0;
@@ -22,7 +21,7 @@ public class Main {
 
     result[0] = cells;
 
-    Rule rule = RuleFactory.getRule("Rule 45");
+    Rule rule = RuleFactory.getRule("Rule 30");
     CellularAutomata cellularAutomata = new CellularAutomata(cells, rule);
 
     for (int i = 1; i < numberOfIterations; i++) {
@@ -30,15 +29,7 @@ public class Main {
       result[i] = cellularAutomata.getCells();
     }
 
-    for (int[] row : result) {
-      for (int state : row) {
-        if (state == 1)
-          Printer.print(state + " ", Ansi.Attribute.BOLD, Ansi.FColor.BLACK, Ansi.BColor.BLACK);
-        else
-          Printer.print(state + " ", Ansi.Attribute.REVERSE, Ansi.FColor.WHITE, Ansi.BColor.WHITE);
-      }
-      System.out.println();
-    }
+    Printer.printCells(result);
 
   }
 
--- src/main/java/es/uca/util/Printer.java
+++ src/main/java/es/uca/util/Printer.java
@@ -5,15 +5,27 @@ import com.diogonunes.jcdp.color.api.Ansi;
 
 public class Printer {
 
-    private static ColoredPrinter coloredPrinter;
+  private static ColoredPrinter coloredPrinter;
 
-    static  {
-        coloredPrinter = new ColoredPrinter.Builder(1, false)
-                .foreground(Ansi.FColor.WHITE).background(Ansi.BColor.WHITE)   //setting format
-                .build();
-    }
+  static {
+    coloredPrinter = new ColoredPrinter.Builder(1, false)
+      .foreground(Ansi.FColor.WHITE).background(Ansi.BColor.WHITE)   //setting format
+      .build();
+  }
 
-    public static void print(String text, Ansi.Attribute attribute, Ansi.FColor frontColor, Ansi.BColor backColor) {
-        coloredPrinter.print(text, attribute, frontColor, backColor);
+  public static void printCells(int[][] cells) {
+    for (int[] row : cells) {
+      for (int state : row) {
+        if (state == 1)
+          coloredPrinter.print(state + " ", Ansi.Attribute.BOLD, Ansi.FColor.BLACK, Ansi.BColor.BLACK);
+        else
+          coloredPrinter.print(state + " ", Ansi.Attribute.REVERSE, Ansi.FColor.WHITE, Ansi.BColor.WHITE);
+      }
+      System.out.println();
     }
+  }
+
+  private static void print(String text, Ansi.Attribute attribute, Ansi.FColor frontColor, Ansi.BColor backColor) {
+    coloredPrinter.print(text, attribute, frontColor, backColor);
+  }
 }
